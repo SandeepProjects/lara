de7bb87e1337f3a501167a2273058e882d8552ec
SandeepProjects <Bagosandeep@gmail.com>
2024-06-11 00:41:27 +0200

    qqq

--- lara10/app/Http/Controllers/UserController.php
+++ lara10/app/Http/Controllers/UserController.php
@@ -40,16 +40,43 @@ class UserController extends Controller
             User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => Hash::make($request->password),  // Hash the password
+                'password' => $request->password
             ]);
 
             return response()->json([
                 'message' => "User successfully created."
-            ], 201);  // Use HTTP status code 201 for resource creation success
+            ], 200);  // Use HTTP status code 201 for resource creation success
         } catch (\Exception $e) {
             return response()->json([
                 'message' => "Something Went Wrong!!",
                 'error' => $e->getMessage()  // Optionally include the error message
+            ], 500);
+        }
+    }
+
+    public function update(UserStoreRequest $request, $id)
+    {
+        try {
+            $users = User::find($id);
+            if (!$users) {
+                return $users()->json([
+                    'message' => 'Users not found!'
+                ], 404);
+            }
+
+            $users->name = $request->name;
+            $users->email = $request->email;
+            $users->password = $request->password;
+
+            $users->save();
+
+            return response()->json([
+                'message' => "User Successfully updated."
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something Went really Wrong!'
             ], 500);
         }
     }
--- lara10/routes/api.php
+++ lara10/routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('users', [UserController::class, 'index']);
 Route::get('users/{id}', [UserController::class,'show']);
 Route::post('addnew', [UserController::class,'store']);
+Route::put('usersupdate/{id}', [UserController::class,'update']);
+

d460e3d1f299656e3b62ca8dd53e7f2e3c82ee4b
SandeepProjects <Bagosandeep@gmail.com>
2024-06-10 23:30:22 +0200

    Update

---
+++ lara10/app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illumniate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json([
+            'results' => $users
+        ], 200);
+    }
+
+
+    // public function show($id)
+    // {
+    //     $users = User::find($id);
+    //     if (!$users) {
+    //         return response()->json([
+    //             'message' => 'User not found.'
+    //         ], 404);
+    //     }
+
+    //     return response()->json([
+    //         'users' => $users
+    //     ], 200);
+    // }
+
+    // public function store(UserStoreRequest $request)
+    // {
+    //     try {
+    //         // Create user with hashed password
+    //         User::create([
+    //             'name' => $request->name,
+    //             'email' => $request->email,
+    //             'password' => Hash::make($request->password),  // Hash the password
+    //         ]);
+
+    //         return response()->json([
+    //             'message' => "User successfully created."
+    //         ], 201);  // Use HTTP status code 201 for resource creation success
+    //     } catch (\Exception $e) {
+    //         return response()->json([
+    //             'message' => "Something Went Wrong!!",
+    //             'error' => $e->getMessage()  // Optionally include the error message
+    //         ], 500);
+    //     }
+    // }  //
+}
